09c6a514137c7e7621d3d822fcfc0806d5e43989
SergeyMoshkov <sergejmoshkov@gmail.com>
2023-02-15 05:43:31 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func downloadStockWebpageBody(symbol string) io.ReadCloser {
+	res, err := http.Get("https://finance.yahoo.com/quote/" + symbol)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if res.StatusCode != 200 {
+		log.Fatalf("status code %d: %s", res.StatusCode, res.Status)
+	}
+
+	return res.Body
+}
+
+const (
+	symbol = "TSLA"
+)
+
+func main() {
+	body := downloadStockWebpageBody(symbol)
+	defer body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	getTextByField := func(field string) string {
+		s := "[data-field=" + field + "][data-symbol=" + symbol + "]"
+
+		return doc.Find(s).Text()
+	}
+
+	marketPrice := getTextByField("regularMarketPrice")
+	marketChange := getTextByField("regularMarketChange")
+	marketChangePercent := getTextByField("regularMarketChangePercent")
+
+	if marketPrice == "" || marketChange == "" || marketChangePercent == "" {
+		log.Fatalln("cannot access market price")
+	}
+
+	fmt.Printf(
+		"%s\n%s\n\t %s\n\t%s\n",
+		symbol,
+		marketPrice,
+		marketChange,
+		marketChangePercent,
+	)
+}
